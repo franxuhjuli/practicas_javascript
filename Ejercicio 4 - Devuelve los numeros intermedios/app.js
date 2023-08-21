2f229c8c2a13a2bd3fbcf032e5940b2636933edd
franxuhjuli <52677153+franxuhjuli@users.noreply.github.com>
2023-08-21 15:01:34 +0200

    Add files via upload

--- Ejercicio 4 - Devuelve los numeros intermedios/app.js
+++ Ejercicio 4 - Devuelve los numeros intermedios/app.js
@@ -1,28 +1,54 @@
-const resultado = document.getElementById('resultado');
-const boton = document.getElementById('boton');
+const resultado = document.getElementById("resultado");
+const boton = document.getElementById("boton");
 
 function calcularNumerosIntermedios() {
-    let valueA = document.getElementById('valor1').value;
-    let valueB = document.getElementById('valor2').value;
-    let resto = Math.abs(valueA - valueB);
-    let numerosIntermedios = [];
+  let valueA = document.getElementById("valor1").value;
+  let valueB = document.getElementById("valor2").value;
+  let resto = Math.abs(valueA - valueB);
 
-    if (valueA === valueB) {
-        resultado.textContent = "Los numeros son iguales";
-    } else {
-        for (let i = resto - 1; i !== 0; i--) {
-            if (valueA < valueB) {
-                valueA++
-                numerosIntermedios.push(valueA);
-            } else {
-                valueB++
-                numerosIntermedios.push(valueB);
-            }
-        }
-        resultado.textContent = `Resultado: ${numerosIntermedios.join(', ')}.`;
+  let numerosIntermedios = [];
+
+  if (valueA === valueB) {
+    resultado.textContent = "Los numeros son iguales";
+  } else if (
+    Number(valueA) + 1 === Number(valueB) ||
+    Number(valueA) - 1 === Number(valueB)
+  ) {
+    resultado.textContent = "Los numeros son contiguos";
+  } else {
+    for (let i = resto - 1; i !== 0; i--) {
+      if (Number(valueA) < Number(valueB)) {
+        valueA++;
+        numerosIntermedios.push(valueA);
+      } else if (Number(valueB) < Number(valueA)) {
+        valueB++;
+        numerosIntermedios.push(valueB);
+      }
     }
+    resultado.textContent = `Resultado: ${numerosIntermedios.join(", ")}.`;
+
+    //Crear boton para calcular solo impares
+    if (document.querySelector(".impares") == null) {
+      const botonImpares = document.createElement("button");
+      document
+        .querySelector(".container")
+        .insertBefore(botonImpares, resultado);
+      botonImpares.textContent = "Solo impares";
+      botonImpares.classList.add("impares");
+      botonImpares.addEventListener("click", calcularImpares);
+    }
+  }
+
+  function calcularImpares() {
+    function esImpar(value) {
+      return value % 2 == 1;
+    }
+
+    resultado.textContent = `Resultado(impares): ${numerosIntermedios
+      .filter(esImpar)
+      .join(", ")}.`;
+    document.querySelector(".impares").remove();
+  }
 }
 
-boton.addEventListener('click', calcularNumerosIntermedios);
-
-
+boton.addEventListener("click", calcularNumerosIntermedios);
